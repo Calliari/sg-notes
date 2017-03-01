71b8a06f6697524f19f29228321d40a88efeb503
Calliari <ocaliari@spartaglobal.co>
2017-03-01 16:06:40 +0000

    add  event to DOM

--- week03-day03-dom-events-jquery/main.js
+++ week03-day03-dom-events-jquery/main.js
@@ -1,23 +1,25 @@
 console.log('in main.js') ;
 
 //console.log(document) ;
+document.addEventListener('DOMContentLoaded', function (){
 
-console.log('--- using querySelectorAll');
 
-var selectedListItems = document.querySelectorAll('.selected');
+  console.log('--- using querySelectorAll');
 
-console.log('selected:', selectedListItems);
+  var selectedListItems = document.querySelectorAll('.selected');
 
-for (var i = 0; i < selectedListItems.length; i++) {
-  console.log('select the list item style', selectedListItems[i].style);
-  selectedListItems[i].style.color = 'red';
-}
+  console.log('selected:', selectedListItems);
 
-console.log('--- Create list item style');
-var newListItem = document.createElement('li') ;
+  for (var i = 0; i < selectedListItems.length; i++) {
+    console.log('select the list item style', selectedListItems[i].style);
+    selectedListItems[i].style.color = 'red';
+  }
 
-newListItem.innerHTML = 'in new York';
-newListItem.setAttribute('class', 'muted');
+  console.log('--- Create list item style');
+  var newListItem = document.createElement('li') ;
+
+  newListItem.innerHTML = 'in new York';
+  newListItem.setAttribute('class', 'muted');
 
 // we are going to call .appendChild on the parent <ul> element.
 // That <ul> Element has an id attribute
@@ -27,9 +29,9 @@ newListItem.setAttribute('class', 'muted');
 // NOTE: wo do NOT pass a CSS selector ('#item-list') to the
 // .getElementById() method.
 
-var listContainer = document.getElementById('item-list') ;
+  var listContainer = document.getElementById('item-list') ;
 
-listContainer.appendChild(newListItem) ;
+  listContainer.appendChild(newListItem) ;
 
 // TODO:
 // - update the above code to add the "selected" CSS class to newListItem
@@ -37,3 +39,18 @@ listContainer.appendChild(newListItem) ;
 
 //newListItem.setAttribute('class', 'setBlueColor');
 //newListItem.style.color = 'blue';
+
+  console.log('--- DOM events') ;
+
+  var pickMeButton = document.getElementById('pickMeBtn') ;
+
+  function handdleButtonClick() {
+    alert('Picked!');
+    pickMeButton.innerHTML = 'You have clicked this buttom';
+    pickMeButton.style.color = 'White';
+    pickMeButton.style.backgroundColor = 'Green';
+  }
+
+  pickMeButton.addEventListener('click', handdleButtonClick);
+
+});
